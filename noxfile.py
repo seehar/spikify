fb75e599982d923c20ae66b83df9b3554e17ff90
seehar <seehar@qq.com>
2025-01-29 18:20:08 +0800

    ci: add multi version test

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+
+@nox.session(python=["3.8", "3.9", "3.11", "3.12"], venv_backend="conda")
+def pytest(session):
+    session.log(f"My python is {session.python}")
+    session.install("poetry")
+    session.run_always("poetry", "install", "--no-root", external=True)
+    session.run("poetry", "install", "--all-extras", "--no-root")
+    session.run("poetry", "run", "pytest", "-s")
