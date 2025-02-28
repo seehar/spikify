8d174c208c80d5fb76a2244ebda955c1ea3f6695
seehar <seehar@qq.com>
2025-02-28 11:24:54 +0800

    fix: 修复nox卡主的问题

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,13 @@ import nox
 
 @nox.session(python=["3.8", "3.9", "3.11", "3.12"], venv_backend="conda")
 def pytest(session):
-    session.log(f"My python is {session.python}")
     session.install("poetry")
-    session.run_always("poetry", "install", "--no-root", external=True)
+
+    # 设置 Poetry 使用当前环境
+    session.env["POETRY_VIRTUALENVS_IN_PROJECT"] = "false"
+    session.env["POETRY_VIRTUALENVS_CREATE"] = "false"
+
+    # session.run_always("poetry", "install", "--no-root", external=True)
     session.run("poetry", "install", "--all-extras", "--no-root")
-    session.run("poetry", "run", "pytest", "-s")
+    session.run("poetry", "run", "pytest", "-s", "--forked")
+    session.run("poetry", "env", "remove", "--python", session.python, external=True)
